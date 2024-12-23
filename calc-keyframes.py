f2daf63102d76f2bf69849562176e474486c5c1b
Cameron Pittman <cameron.w.pittman@gmail.com>
2024-12-22 23:22:56 -0500

    wip - all css animations

---
+++ calc-keyframes.py
@@ -0,0 +1,43 @@
+num_stops = 60
+percent_time_scrolling = 0.25
+
+percent_time_on_score = 1 - percent_time_scrolling
+
+tick_time = 100 / num_stops
+time_per_scrolling = tick_time * percent_time_scrolling
+time_per_keyframe = tick_time * percent_time_on_score
+
+margin_at_end = 100 - (((num_stops + 1) * tick_time) + time_per_keyframe)
+extra_buffer = margin_at_end / num_stops
+
+first = f"""0% {{
+  transform: translateY(calc(var(--ticker-height) * 1));
+  opacity: 0;
+}}
+"""
+last = f"""100% {{
+  transform: translateY(calc(var(--ticker-height) * -{num_stops}));
+  opacity: 0;
+}}
+"""
+vals = [first]
+
+last_end = 0
+for i in range(0, num_stops + 1):
+    start = last_end + time_per_scrolling
+    end = start + time_per_keyframe + extra_buffer
+
+    last_end = end
+
+    keyframe = f"""{start}%,
+{end}% {{
+  transform: translateY(calc(var(--ticker-height) * -{i}));
+  opacity: 1;
+}}
+"""
+
+    vals.append(keyframe)
+
+vals.append(last)
+
+print("\n".join(vals))
