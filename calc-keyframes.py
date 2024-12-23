fec51b044bbf708278403a6c108753b1b6e14b05
Cameron Pittman <cameron.w.pittman@gmail.com>
2024-12-23 12:26:26 -0500

    sync league logos with scores; start adding more real info

--- calc-keyframes.py
+++ calc-keyframes.py
@@ -1,5 +1,7 @@
-num_stops = 60
-percent_time_scrolling = 0.25
+# set these
+num_stops = 3
+percent_time_scrolling = 0.05
+
 
 percent_time_on_score = 1 - percent_time_scrolling
 
@@ -7,8 +9,8 @@ tick_time = 100 / num_stops
 time_per_scrolling = tick_time * percent_time_scrolling
 time_per_keyframe = tick_time * percent_time_on_score
 
-margin_at_end = 100 - (((num_stops + 1) * tick_time) + time_per_keyframe)
-extra_buffer = margin_at_end / num_stops
+# margin_at_end = 100 - (((num_stops + 1) * tick_time) + time_per_keyframe)
+# extra_buffer = margin_at_end / num_stops
 
 first = f"""0% {{
   transform: translateY(calc(var(--ticker-height) * 1));
@@ -23,9 +25,9 @@ last = f"""100% {{
 vals = [first]
 
 last_end = 0
-for i in range(0, num_stops + 1):
+for i in range(0, num_stops):
     start = last_end + time_per_scrolling
-    end = start + time_per_keyframe + extra_buffer
+    end = start + time_per_keyframe
 
     last_end = end
 
